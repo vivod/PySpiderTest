34d0d2e28a6e0b5b3d4bc595fc2a4c9e10e864ba
anonymouss <aquarius.cjj@gmail.com>
2015-05-31 22:06:30 +0800

    更新，可以抓取具体主题内的图片了

--- LoveBridge.py
+++ LoveBridge.py
@@ -1,19 +1,34 @@
-# -*- coding:utf-8 -*-
+# -*- coding: UTF-8 -*-
+
+import urllib
 import urllib2
 import re
-
-# https://bbs.sjtu.edu.cn/bbstdoc,board,LoveBridge.html  LoveBridge URL
+import os
 
 class LoveBridge:
     def __init__(self):
-        self.url = 'https://bbs.sjtu.edu.cn/bbstdoc,board,LoveBridge.html'
+        # 主站URL
+        self.siteURL = "https://bbs.sjtu.edu.cn/"
+        # LB 板URL
+        self.lburl = self.siteURL + 'bbstdoc,board,LoveBridge.html'
+        # 伪装浏览器
         self.user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
-        self.headers = { 'User-Agent' : self.user_agent }
-    def getPage(self):
+        self.headers = {'User-Agent': self.user_agent}
+        # 保存当前页面的各条主题URL
+        self.subjects = []
+        # 保存当前页面前一页的URL
+        self.preurl = ""
+
+    # 获得指定页面（URL）的HTML文件
+    def getPage(self, url, subject):
         try:
-            request = urllib2.Request(self.url, headers = self.headers)
+            request = urllib2.Request(url, headers=self.headers)
             response = urllib2.urlopen(request)
-            pageCode = response.read().decode('gbk')
+            # 为什么具体主题页不使用GBK编码，因为尝试了不行。。。
+            if subject:
+                pageCode = response.read()
+            else:
+                pageCode = response.read().decode('gbk')
             return pageCode
         except urllib2.URLError, e:
             if hasattr(e, 'code'):
@@ -22,8 +37,10 @@ class LoveBridge:
                 print e.reason
             print u'连接失败'
             return None
-    def getPageItems(self):
-        pageCode = self.getPage()
+
+    # 获取页面中各条主题的URL
+    def getPageItems(self, url):
+        pageCode = self.getPage(url, False)
         if not pageCode:
             print u'页面加载失败'
             return None
@@ -34,15 +51,64 @@ class LoveBridge:
         for inform in informs:
             haveOther = re.search("font", inform[0])
             if not haveOther:
-                print inform[0],'    ', inform[1],'    ', inform[2]
+                print inform[0], '    ', inform[1], '    ', inform[2]
+                self.subjects.append(inform[1].strip())
         for page in prepage:
             pre = re.search(u"上一页", page[1])
             if pre:
                 print page[0], '    ', page[1]
-    def start(self):
+                self.preurl = page[0]
+
+    # 获取具体主题页面的图片URL
+    def getPics(self, url):
+        pageCode = self.getPage(url, True)
+        if not pageCode:
+            print u'页面加载失败'
+            return None
+        pattern = re.compile('<IMG SRC="/file/LoveBridge/(.*?).jpg"', re.S)
+        subjs = re.findall(pattern, pageCode)
+        for s in subjs:
+            s = s.split(".")[0]
+            imgUrl = 'https://bbs.sjtu.edu.cn/file/LoveBridge/' + s +".jpg"
+            self.saveImage(imgUrl, 'Images/' + s + '.jpg')
+
+    # 创建文件夹
+    def mkdir(self, path):
+        path = path.strip()
+        isExist = os.path.exists(path)
+        if not isExist:
+            print u"创建了文件夹:" , path , u"保存图片"
+            os.mkdir(path)
+            return True
+        else:
+            return False
+
+    # 保存图片
+    def saveImage(self, url, fname):
+        u = urllib.urlopen(url)
+        data = u.read()
+        f = open(fname, 'wb')
+        f.write(data)
+        f.close()
+
+    # 方法入口
+    def start(self, pages):
         print u'读取饮水思源BBS，LoveBridge板信息'
-        self.getPageItems()
+        self.mkdir("Images")
+        if pages < 1:
+            print u"查询页面必须大于1"
+            return None
+        else:
+            url = self.lburl
+            for i in range(pages):
+                print u"打开第 " , i+1 , u" 个页面， " , url
+                self.getPageItems(url)
+                for subj in self.subjects:
+                    self.getPics(self.siteURL + subj)
+                url = self.siteURL + self.preurl
 
-
+# 创建对象
 lb = LoveBridge()
-lb.start()
+# 设置需要抓取多少个页面的图片
+lb.start(10)
+
--- README.md
+++ README.md
@@ -1,5 +1,18 @@
+#### Create 2015.5.31
+---
 一个简单的Python爬虫，目前只能抓取[bbs.sjtu.edu.cn](bbs.sjtu.edu.cn)LoveBridge首页信息
 可以抓取到每条主题的ID, URL, Title。 以及前一页的URL
 接下来可以根据这些信息抓取每个主题的内容，图片，以及前一页面的这些信息
 
-#### reference: [Python爬虫学习系列教程](http://cuiqingcai.com/1052.html)
+###### reference: [Python爬虫学习系列教程](http://cuiqingcai.com/1052.html)
+---
+#### update 2015.5.31
+---
+用法
+```python
+# 创建对象
+lb = LoveBridge()
+# 设置需要抓取多少个页面的图片
+lb.start(10)
+```
+基本可以抓取到各主题页面的图片，但还有些bug
